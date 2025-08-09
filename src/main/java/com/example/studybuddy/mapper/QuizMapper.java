fffda5d580ae6c2aa6e75a5b72d5d6b7344f6269
xnice1 <dude.crazyme@gmail.com>
2025-08-09 02:46:07 +0200

    added QuizMapper

---
+++ src/main/java/com/example/studybuddy/mapper/QuizMapper.java
@@ -0,0 +1,21 @@
+package com.example.studybuddy.mapper;
+
+import com.example.studybuddy.dto.QuizDTO;
+import com.example.studybuddy.model.Quiz;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface QuizMapper {
+
+    QuizMapper INSTANCE = Mappers.getMapper(QuizMapper.class);
+
+    Quiz toEntity(QuizDTO dto);
+
+    @Mapping(target = "courseId", source = "course.id")
+    QuizDTO toDto(Quiz quiz);
+
+    @Mapping(target = "course.id", source = "courseId")
+    Quiz fromDto(QuizDTO dto);
+}
