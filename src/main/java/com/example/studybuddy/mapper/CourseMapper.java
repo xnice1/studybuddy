32a60315dbfee0283469a8ab41508f1337448e30
xnice1 <dude.crazyme@gmail.com>
2025-09-14 21:35:14 +0200

    fixing CI

--- src/main/java/com/example/studybuddy/mapper/CourseMapper.java
+++ src/main/java/com/example/studybuddy/mapper/CourseMapper.java
@@ -21,7 +21,6 @@ public interface CourseMapper {
     CourseDTO toDto(Course course);
 
     @Mapping(target = "owner", source = "ownerId",qualifiedByName = "idToOwner")
-    @Mapping(target = "quizzes",source = "id")
     @Mapping(target = "id", source = "id")
     Course fromDto(CourseDTO dto);
 
