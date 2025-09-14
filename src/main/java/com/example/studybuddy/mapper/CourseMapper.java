579dafe75363e2f8634ac15806144fb8c94419d0
xnice1 <dude.crazyme@gmail.com>
2025-09-14 21:07:30 +0200

    fixing CI

--- src/main/java/com/example/studybuddy/mapper/CourseMapper.java
+++ src/main/java/com/example/studybuddy/mapper/CourseMapper.java
@@ -2,22 +2,32 @@ package com.example.studybuddy.mapper;
 
 import com.example.studybuddy.dto.CourseDTO;
 import com.example.studybuddy.model.Course;
+import com.example.studybuddy.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
 
-    CourseMapper INSTANCE = Mappers.getMapper(CourseMapper.class);
 
+    @Mapping(target ="owner",source ="ownerId",qualifiedByName = "idToOwner")
     Course toEntity(CourseDTO dto);
 
     @Mapping(target = "ownerId", source = "owner.id")
     CourseDTO toDto(Course course);
 
-    @Mapping(target = "owner.id", source = "ownerId")
+    @Mapping(target = "owner", source = "ownerId")
     @Mapping(target = "id", source = "id")
     Course fromDto(CourseDTO dto);
+
+    @Named("idToOwner")
+        default User idToOwner(Long id) {
+        if (id == null) return null;
+        User c = new User();
+        c.setId(id);
+        return c;
+    }
 }
