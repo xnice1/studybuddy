d8cd62d09d5c067e5ef9642a0a120f18ed457f7a
xnice1 <dude.crazyme@gmail.com>
2025-09-14 20:17:08 +0200

    fixing CI

--- src/main/java/com/example/studybuddy/mapper/QuizMapper.java
+++ src/main/java/com/example/studybuddy/mapper/QuizMapper.java
@@ -10,12 +10,12 @@ import org.mapstruct.factory.Mappers;
 public interface QuizMapper {
 
     QuizMapper INSTANCE = Mappers.getMapper(QuizMapper.class);
-    @Mapping(target = "courseId", source = "courseId")
+
     Quiz toEntity(QuizDTO dto);
 
-    @Mapping(target = "courseId", source = "courseId")
+    @Mapping(target = "courseId", source = "course.id")
     QuizDTO toDto(Quiz quiz);
 
-    @Mapping(target = "courseId", source = "courseId")
+    @Mapping(target = "course.id", source = "courseId")
     Quiz fromDto(QuizDTO dto);
 }
